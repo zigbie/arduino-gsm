8b09e55725d3cb56c57b81323d208c8114da5c3d
zigbie <zigbie@gmail.com>
2018-01-21 17:57:42 +0300

    fix

--- gsm.ino
+++ gsm.ino
@@ -12,74 +12,86 @@ unsigned long workTime = 600000; // work time (20 min = 1200000)
 unsigned long startTime;  // button ON or Ring
 unsigned long currentTime; // millis();
 
-void setup(){
+String val = "";
+int ch = 0;
+
+void setup() {
 
   pinMode(Relay, OUTPUT);
   digitalWrite(Relay, state);
   pinMode(buttonPin, INPUT_PULLUP);
   gsm.begin(57600);
+  Serial.begin(57600);
   gsm.println("AT+CLIP=1");
 }
 
 void loop() {
 
-  if(gsm.find("RING")){                      // if RING
-    gsm.println("AT+CLIP=1");              // включаем АОН,
-
-    while(1){
-      if (gsm.find("37529XXXXXXX")){         // if phone number = TRUE
-        startTime = millis();
-        gsm.println("ATH");           // off Ring
-        break;
-      } else if (gsm.find("37529XXXXXXX")){
-        startTime = millis();
-        gsm.println("ATH");
-        break;
-      } else if (gsm.find("37529XXXXXXX")){
-        startTime = millis();
-        gsm.println("ATH");
-        break;
-      } else {
-        gsm.println("AT+CPAS");             // modem state
-        delay(100);
-        if (gsm.find("+CPAS: 0")) break;    // end cycle
-     }
+  if (gsm.available()) {
+    while (gsm.available()) {
+      ch = gsm.read();
+      val += char(ch);
+      delay(10);
     }
+
+    if (val.indexOf("RING") > -1) {
+      //Serial.println("RING");
+      //Serial.println(val);
+      if ((val.indexOf("375291234567") > -1) || (val.indexOf("375291234567") > -1)) {
+        //  Serial.println("--- MASTER RING DETECTED ---");
+        startTime = millis();
+        gsm.println("ATH0");
+        delay(500);
+      } else {
+        gsm.println("ATH0");
+      }
+    }
+    val = "";
   }
 
 
-  buttonState = digitalRead(buttonPin);
+  buttonState = readButton(buttonPin);
 
-  if (buttonState == LOW && state == LOW) {  // button ON, relay ON
-    delay(100);
-    startTime = 0;
+  if (buttonState == LOW) {
+    if (state == LOW) {  // button ON, relay ON
+      startTime = 0;
+    }
+
+    if (state == HIGH) {  // button ON, relay OFF
+      startTime = millis();
+    }
+    delay(500);
   }
-
-  if (buttonState == LOW && state == HIGH) {  // button ON, relay OFF
-    delay(100);
-    startTime = millis();
-  }
-
   state = runWebasto (startTime);
   digitalWrite(Relay, state);
 
 }
 
+int readButton(int pin)
+{
+  if (digitalRead(pin) == LOW) {
+    delay(100);
+    if (digitalRead(pin) == LOW) {
+      return LOW;
+    }
+  }
+  return HIGH;
+}
 
 int runWebasto (unsigned long startTime) {
   currentTime = millis();
-  if(startTime == 0){
-  state  = HIGH;
-  return  state;
+  if (startTime == 0) {
+    state  = HIGH;
+    return  state;
   }
 
-  if((currentTime - startTime) > workTime){        // work: yes or no
+  if ((currentTime - startTime) > workTime) {      // work: yes or no
     state  = HIGH;
   } else {
-    if(startTime > 2000){
-    state  = LOW;
+    if (startTime > 2000) {
+      state  = LOW;
     }
   }
 
-return  state;
+  return  state;
 }
